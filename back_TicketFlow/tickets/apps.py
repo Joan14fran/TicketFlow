b09530bab679818c12179e6c56d8060cb7dc6a75
j0andev <santiago.cuart@gmail.com>
2025-12-02 15:58:27 -0500

    feat: automatically create a default 'Soporte General' category on app migration.

--- back_TicketFlow/tickets/apps.py
+++ back_TicketFlow/tickets/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_default_category(sender, **kwargs):
+    """Crea la categoría por defecto al iniciar la app"""
+    from .models import Category
+    if not Category.objects.filter(name='Soporte General').exists():
+        Category.objects.create(name='Soporte General')
+        print("--- Categoría 'Soporte General' creada automáticamente ---")
 
 class TicketsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tickets'
+
+    def ready(self):
+        post_migrate.connect(create_default_category, sender=self)
